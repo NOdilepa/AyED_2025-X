3b99a087e549cb932be05aac2a3dc9f543daeea7
NOdilepa <nahuelpaloni@gmail.com>
2025-05-15 08:51:26 -0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Nahuel_Paloni_
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            double num1, fahrenheit;
+
+            Console.Write("Ingresa el primer número: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            {
+
+                fahrenheit = (num1 * 1.8 + 32);
+            }
+
+            Console.WriteLine(" El valor fahrenheit es " + fahrenheit);
+            Console.ReadKey();
+
+        }
+    }
+}
